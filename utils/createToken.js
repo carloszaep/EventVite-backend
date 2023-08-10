d65f3ed47d3a85409fa642702f11c0e5c65fd7c2
carloszaep <106843133+carloszaep@users.noreply.github.com>
2023-08-09 22:01:24 -0400

    change token to 8 character

--- utils/createToken.js
+++ utils/createToken.js
@@ -1,5 +1,5 @@
 import crypto from 'crypto'
 
 export function createToken () {
-  return crypto.randomBytes(8).toString('hex').toUpperCase()
+  return crypto.randomBytes(4).toString('hex').toUpperCase()
 }
